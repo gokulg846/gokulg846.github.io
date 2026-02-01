fd127d9e07a65e8b0a80f7d450d6530b0aec8600
gokulg846 <52713531+gokulg846@users.noreply.github.com>
2026-02-01 21:09:47 +0000

    Deploying to gh-pages from @ gokulg846/gokulg846.github.io@849abfd4ff16f9e199fcc6ba893ae615321610a0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gokulg846/gokulgopal.github.io",
+    "data-repo": "gokulg846/gokulg846.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
